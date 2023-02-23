bfbe3ce4082e2113d91b17dcd10e4ae1ba872ed5
Mesut Aydın <xmesut123@gmail.com>
2023-02-23 12:58:17 +0300

    Added command in kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,16 +2,25 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SyncStatusCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        SyncStatusCommand::class
+    ];
+
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('sync:status')
+            ->everyMinute()
+            ->withoutOverlapping()
+            ->after(fn() => $this->call('queue:retry all --tries=3'));
         // $schedule->command('inspire')->hourly();
     }
 
